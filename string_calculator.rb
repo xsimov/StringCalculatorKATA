0467a8f01aaead85e35d66ba85112e1ad071431f
Xavier Simó <xsimov@gmail.com>
2014-09-20 23:28:05 +0200

    Refactor

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,16 +1,25 @@
 class StringCalculator
 
   def calculate(input_string)
-    if input_string.include? "+"
-      operands = input_string.split("+")
-      result = operands.first.to_i + operands.last.to_i
-    elsif input_string.include? "*"
-      operands = input_string.split("*")
-      result = operands.first.to_i * operands.last.to_i
+    @input_string = input_string
+    if @input_string.include? "+"
+      sum
+    elsif @input_string.include? "*"
+      multiplication
     else
-      result = input_string
+      @result = @input_string
     end
-    result.to_s
+    @result.to_s
+  end
+
+  def sum
+    operands = @input_string.split("+")
+    @result = operands.first.to_i + operands.last.to_i
+  end
+
+  def multiplication
+    operands = @input_string.split("*")
+    @result = operands.first.to_i * operands.last.to_i
   end
 
 end
