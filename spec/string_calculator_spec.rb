e7e8115d543469c867dcf480e6491409363bafbb
Xavier Simó <xsimov@gmail.com>
2014-09-20 22:50:56 +0200

    Green

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -7,6 +7,7 @@ describe "string calculator" do
 
   it "outputs the same input if its a number" do
     expect(StringCalculator.new.calculate("43")).to eq("43")
+    expect(StringCalculator.new.calculate("22")).to eq("22")
   end
 
   it "outputs the result of an input sum" do
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,8 +1,8 @@
 class StringCalculator
 
   def calculate(input_string)
-    if input_string == "" || input_string == "43"
-      result = (input_string == "") ? "" : "43"
+    unless input_string.include?("+")
+      result = input_string
     else
       result = input_string[0].to_i + input_string[2].to_i  
       result.to_s
